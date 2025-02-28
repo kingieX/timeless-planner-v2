ff8e96251b3ab965e5afaf2895c1791939d67ff0
nnachi kingsley <chimakingsley216@gmail.com>
2025-02-28 18:27:52 +0100

    Update:verify otp page done

--- src/components/AOSProvider.tsx
+++ src/components/AOSProvider.tsx
@@ -7,7 +7,7 @@ import "aos/dist/aos.css";
 export default function AOSProvider() {
   useEffect(() => {
     AOS.init({
-      duration: 600, // Animation duration
+      // duration: 600, // Animation duration
       once: false, // Ensures animation runs every time it enters the viewport
       // easing: "ease-in-out",
     });
