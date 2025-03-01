f79cb8b9a4cc4e4446015b49aa1625e5eb755d68
nnachi kingsley <chimakingsley216@gmail.com>
2025-03-01 14:58:27 +0100

    Update:project page and components in progress

---
+++ src/app/(app)/projects/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "../dashboard/layout";
+
+const page = () => {
+  return (
+    <DashboardLayout>
+      <div className="py-6 mt-4">
+        <h2 className="md:text-2xl font-semibold mb-4">Projects</h2>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default page;
