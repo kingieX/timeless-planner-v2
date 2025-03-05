6c53419ee340eb0b8ca2d091e1e3db20b2ed9d65
nnachi kingsley <chimakingsley216@gmail.com>
2025-03-05 23:50:35 +0100

    Update:project page and components in progress

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  compilerOptions: {
-    skipLibCheck: true,
-    noImplicitAny: false,
-  },
 };
 
 export default nextConfig;
